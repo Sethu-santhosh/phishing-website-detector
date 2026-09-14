5dd7204412daa01c416ff5c6776b1ba01334dd3c
Sethu-santhosh <sethusanthosh2621@gmail.com>
2026-09-13 21:10:51 -0700

    Refactor URL patterns to use direct imports

--- checker/urls.py
+++ checker/urls.py
@@ -1,9 +1,28 @@
+from django.contrib import admin
 from django.urls import path
-from . import views
-urlpatterns=[
-    path("",views.home,name="home"),
-    path("register/",views.register,name="register"),
-    path("login/",views.user_login,name="login"),
-    path("logout/",views.user_logout,name="logout"),
-    path("history/",views.history,name="history"),
+
+from checker.views import (
+    home,
+    register,
+    user_login,
+    user_logout,
+    history,
+    dashboard,
+)
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+
+    path("", home, name="home"),
+
+    path("register/", register, name="register"),
+
+    path("login/", user_login, name="login"),
+
+    path("logout/", user_logout, name="logout"),
+
+    path("history/", history, name="history"),
+
+    path("dashboard/", dashboard, name="dashboard"),
 ]
